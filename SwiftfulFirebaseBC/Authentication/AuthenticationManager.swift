d7fb50535d570e55300dc09bd26cda19ba98fff5
n-sivaram <sinakka@fiskerinc.com>
2023-03-25 08:39:37 +0530

    Implemented Firebase authentication functions like sign up, sign in, sign out, reset password, update password and update email.

--- SwiftfulFirebaseBC/Authentication/AuthenticationManager.swift
+++ SwiftfulFirebaseBC/Authentication/AuthenticationManager.swift
@@ -59,4 +59,24 @@ final class AuthenticationManager {
     func signOut() throws {
         try Auth.auth().signOut()
     }
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    }
+    
+    func updatePassword(password: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        try await user.updatePassword(to: password)
+    }
+    
+    func updateEmail(email: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        try await user.updateEmail(to: email)
+    }
 }
--- SwiftfulFirebaseBC/Authentication/SettingsView.swift
+++ SwiftfulFirebaseBC/Authentication/SettingsView.swift
@@ -13,6 +13,26 @@ final class SettingsViewModel: ObservableObject {
     func signOut() throws {
         try AuthenticationManager.shared.signOut()
     }
+    
+    func resetPassword() async throws {
+        let authUser = try AuthenticationManager.shared.getAuthenticatedUser()
+        guard let email = authUser.email else {
+            throw URLError(.fileDoesNotExist)
+        }
+        
+        try await AuthenticationManager.shared.resetPassword(email: email)
+    }
+    
+    func updateEmail() async throws {
+        let email = "hello123@gmail.com"
+        try await AuthenticationManager.shared.updateEmail(email: email)
+    }
+    
+    func updatePassword() async throws {
+        let password = "Hello123!"
+        try await AuthenticationManager.shared.updatePassword(password: password)
+    }
+
 }
 
 struct SettingsView: View {
@@ -33,6 +53,8 @@ struct SettingsView: View {
                     }
                 }
             }
+            
+            emailSection
         }
         .navigationTitle("Settings")
     }
@@ -42,6 +64,48 @@ struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             SettingsView(showSignInView: .constant(false))
+        }
+    }
+}
+
+
+extension SettingsView {
+    private var emailSection: some View {
+        Section {
+            Button("Reset password") {
+                Task {
+                    do {
+                        try await viewModel.resetPassword()
+                        print("PASSWORD RESET!")
+                    } catch {
+                        print("Error: ", error.localizedDescription)
+                    }
+                }
+            }
+
+            Button("Update password") {
+                Task {
+                    do {
+                        try await viewModel.updatePassword()
+                        print("PASSWORD UPDATED!")
+                    } catch {
+                        print("Error: ", error.localizedDescription)
+                    }
+                }
+            }
+
+            Button("Update email") {
+                Task {
+                    do {
+                        try await viewModel.updateEmail()
+                        print("EMAIL UPDATED!")
+                    } catch {
+                        print("Error: ", error.localizedDescription)
+                    }
+                }
+            }
+        } header: {
+            Text("Email functions")
         }
     }
 }
